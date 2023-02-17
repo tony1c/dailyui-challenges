825470dd539a2ae527cd43276f4003b10bd1d8e6
tony1c <antoniomario.cavuto@gmail.com>
2023-02-17 14:10:05 +0100

    sign up #001

---
+++ sign-up#001/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        cream: "#FDFCFB",
+        gold: "#E3D2C4",
+      },
+    },
+  },
+  plugins: [],
+};
