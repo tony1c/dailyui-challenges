3bd57b628fb85966d485650540811cd1d593f014
tony1c <antoniomario.cavuto@gmail.com>
2023-02-18 18:23:37 +0100

    cc-checkout 002

---
+++ cc-checkout-two/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        // Primary
+        color1: "#74ebd5",
+        color2: "#ACB6E5",
+      },
+    },
+  },
+  plugins: [],
+};
