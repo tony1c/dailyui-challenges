6471d2b2abb72865ac5971b954081b75b4860f24
tony1c <antoniomario.cavuto@gmail.com>
2023-02-18 14:50:06 +0100

    sign up #001

---
+++ sign-up-one/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        violet: "rgba(98,58,162,1)",
+        peach: "rgba(249,151,119,1)",
+        darkBlue: "#111727",
+      },
+    },
+  },
+  plugins: [],
+};
