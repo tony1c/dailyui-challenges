f59953690029116fdd6b46e377dd26041202f3dd
tony1c <antoniomario.cavuto@gmail.com>
2023-02-20 13:26:58 +0100

    cc-checkout 002 finish

--- cc-checkout-two/tailwind.config.js
+++ cc-checkout-two/tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         color1: "#74ebd5",
         color2: "#ACB6E5",
         // Secondary
-        color3: "#51a595",
+        color3: "#68d4c0",
         color4: "#565b73",
         lightBg: "#f1fdfb",
       },
